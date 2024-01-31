2d806f0eb291c3a8f87abab5c2554b7f75b7a765
unknown <paradiscaleb@gmail.com>
2024-01-30 23:55:17 -0500

    First upload

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
