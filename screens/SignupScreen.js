a799daf62693c743b718692be9000dd717a72600
Abilaash <abilaash2001@gmail.com>
2024-02-02 11:51:51 -0500

    setup for firebase authentication with alerts when account is created and logging in successfully

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,11 +2,30 @@ import { View, Text, Image, SafeAreaView, TextInput, TouchableOpacity, Pressable
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useNavigation } from '@react-navigation/native'
-import Animated, { FadeIn, FadeInDown, FadeInUp } from 'react-native-reanimated';
-
+import Animated, { FadeIn, FadeInDown, FadeInUp } from 'react-native-reanimated'
+import { FIREBASE_AUTH } from '../config/firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { useState } from 'react'
 
 export default function SignupScreen() {
     const navigation = useNavigation();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+
+    const auth = FIREBASE_AUTH
+
+    const signUpFn = async () => {
+        try {
+            const response = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(response)
+            alert('created account!')
+            navigation.navigate('Login')
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
   return (
     <View className="bg-white h-full w-full">
       <StatusBar style="light" />
@@ -45,7 +64,9 @@ export default function SignupScreen() {
                 className="bg-black/5 p-5 rounded-2xl w-full">
                 <TextInput
                     placeholder="Username"
+                    value={username}
                     placeholderTextColor={'gray'}
+                    onChangeText={(text) => setUsername(text)}
                 />
             </Animated.View>
             <Animated.View 
@@ -53,6 +74,9 @@ export default function SignupScreen() {
                 className="bg-black/5 p-5 rounded-2xl w-full">
                 <TextInput
                     placeholder="Email"
+                    value={email}
+                    autoCapitalize='none'
+                    onChangeText={(text) => setEmail(text)}
                     placeholderTextColor={'gray'}
                 />
             </Animated.View>
@@ -61,13 +85,16 @@ export default function SignupScreen() {
                 className="bg-black/5 p-5 rounded-2xl w-full mb-3">
                 <TextInput
                     placeholder="Password"
+                    value={password}
+                    autoCapitalize='none'
+                    onChangeText={(text) => setPassword(text)}
                     placeholderTextColor={'gray'}
                     secureTextEntry
                 />
             </Animated.View>
 
             <Animated.View className="w-full" entering={FadeInDown.delay(600).duration(1000).springify()}>
-                <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3">
+                <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl mb-3" onPress={signUpFn}>
                     <Text className="text-xl font-bold text-white text-center">SignUp</Text>
                 </TouchableOpacity>
             </Animated.View>
